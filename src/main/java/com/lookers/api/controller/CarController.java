52b38278f05ea08a3ba865212b11b6248656f486
Emil Ivanov <emil@eivanovue.com>
2021-02-16 10:09:32 +0000

    Merge some changes from alex into my own work

--- src/main/java/com/lookers/api/controller/CarController.java
+++ src/main/java/com/lookers/api/controller/CarController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.validation.constraints.Null;
-
 @Controller
 public class CarController {
 

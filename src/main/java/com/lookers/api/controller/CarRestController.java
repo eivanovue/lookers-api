5172c2878097607d95a4d6df1c9904d76d70f155
Emil Ivanov <emil@eivanovue.com>
2021-02-25 10:42:27 +0000

    allow cross origin for all cars rest get

--- src/main/java/com/lookers/api/controller/CarRestController.java
+++ src/main/java/com/lookers/api/controller/CarRestController.java
@@ -5,6 +5,7 @@ import com.lookers.api.model.Car;
 import com.lookers.api.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,7 @@ public class CarRestController {
         }
     }
 
+    @CrossOrigin
     @GetMapping(value = "/cars")
     public List<Car> getCars() {
         return carService.getAllCars();
